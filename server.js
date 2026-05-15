101e8d8ce96ae868e2ed744ea486affc350fbcd2
DarshanBackend <darshan1.kalathiyainfotech@gmail.com>
2026-05-15 12:11:39 +0530

    editProfile in changes 15-05-2026

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
+import 'dotenv/config';
 import express from 'express';
-import dotenv from 'dotenv';
-dotenv.config();
 import cors from 'cors';
 import { connectDB } from './src/config/db.js';
 import indexRouter from './src/routes/indexRoutes.js';
